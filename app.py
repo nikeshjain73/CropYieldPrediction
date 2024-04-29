4b17bdb06490a708ec58ce00726bd320048cea79
Nikesh jain <jnikesh050@gmail.com>
2024-04-29 22:44:25 +0530

    sceond commit

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ def predict():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__=="__main__":
     app.run(debug=True)
